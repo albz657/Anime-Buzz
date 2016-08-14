bef40f24c86b67aaba893d201bc5eeb587b200a8
Jake Moritz <jakemoritz1@gmail.com>
2016-08-13 20:08:15 -0400

    Fixed series matching on initialization

--- app/src/main/java/me/jakemoritz/animebuzz/api/mal/MalImportHelper.java
+++ app/src/main/java/me/jakemoritz/animebuzz/api/mal/MalImportHelper.java
@@ -1,7 +1,12 @@
 package me.jakemoritz.animebuzz.api.mal;
 
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
+
+import java.util.Iterator;
 import java.util.List;
 
+import me.jakemoritz.animebuzz.R;
 import me.jakemoritz.animebuzz.activities.MainActivity;
 import me.jakemoritz.animebuzz.api.mal.models.MatchHolder;
 import me.jakemoritz.animebuzz.data.DatabaseHelper;
@@ -9,6 +14,7 @@ import me.jakemoritz.animebuzz.fragments.MyShowsFragment;
 import me.jakemoritz.animebuzz.fragments.SeriesFragment;
 import me.jakemoritz.animebuzz.helpers.App;
 import me.jakemoritz.animebuzz.interfaces.MalDataRead;
+import me.jakemoritz.animebuzz.models.Season;
 import me.jakemoritz.animebuzz.models.Series;
 import me.jakemoritz.animebuzz.models.SeriesList;
 
@@ -26,23 +32,56 @@ public class MalImportHelper {
 
     public void matchSeries(List<MatchHolder> matchList) {
         SeriesList matchedSeries = new SeriesList();
+        DatabaseHelper dbHelper = null;
 
-        DatabaseHelper dbHelper = DatabaseHelper.getInstance(App.getInstance());
+        if (App.getInstance().isInitializingGotImages()) {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(App.getInstance());
+            String latestSeasonName = sharedPreferences.getString(App.getInstance().getString(R.string.shared_prefs_latest_season), "");
+            Season latestSeason = null;
+            if (!latestSeasonName.isEmpty()) {
+                for (Season season : App.getInstance().getAllAnimeSeasons()) {
+                    if (season.getSeasonMetadata().getName().equals(latestSeasonName)) {
+                        latestSeason = season;
+                        break;
+                    }
+                }
+            }
 
-        for (MatchHolder match : matchList) {
-            Series tempSeries = dbHelper.getSeries(match.getMALID());
+            if (latestSeason != null) {
+                MatchHolder match = null;
+                for (Iterator matchedIterator = matchList.iterator(); matchedIterator.hasNext(); ) {
+                    match = (MatchHolder) matchedIterator.next();
+                    for (Series savedSeries : latestSeason.getSeasonSeries()) {
+                        if (match.getMALID() == savedSeries.getMALID()) {
+                            savedSeries.setInUserList(true);
+                            savedSeries.setEpisodesWatched(match.getEpisodesWatched());
+                            matchedSeries.add(savedSeries);
+                            matchedIterator.remove();
+                            break;
+                        }
+                    }
+                }
 
-            if (tempSeries != null) {
-                tempSeries.setInUserList(true);
-                tempSeries.setEpisodesWatched(match.getEpisodesWatched());
-                matchedSeries.add(tempSeries);
+            }
+        } else {
+            dbHelper = DatabaseHelper.getInstance(App.getInstance());
+
+            for (MatchHolder match : matchList) {
+
+                Series tempSeries = dbHelper.getSeries(match.getMALID());
+
+                if (tempSeries != null) {
+                    tempSeries.setInUserList(true);
+                    tempSeries.setEpisodesWatched(match.getEpisodesWatched());
+                    matchedSeries.add(tempSeries);
 
 //                App.getInstance().getCircleBitmap(tempSeries);
+                }
             }
         }
 
         App.getInstance().getUserAnimeList().addAll(matchedSeries);
-        for (Series series : App.getInstance().getUserAnimeList()){
+        for (Series series : App.getInstance().getUserAnimeList()) {
             if (series.getAirdate() > 0 && series.getSimulcast_airdate() > 0) {
                 App.getInstance().makeAlarm(series);
             }
@@ -57,9 +96,11 @@ public class MalImportHelper {
             ((MyShowsFragment) fragment).loadUserSortingPreference();
         }
 
-        dbHelper.saveSeriesList(App.getInstance().getUserAnimeList());
+        if (dbHelper != null) {
+            dbHelper.saveSeriesList(App.getInstance().getUserAnimeList());
+        }
 
-        if (delegate != null){
+        if (delegate != null) {
             delegate.malDataRead();
         }
     }
