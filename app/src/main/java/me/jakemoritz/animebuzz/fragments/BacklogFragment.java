e7e538b0a836538f6f0940b0d3e9f35b9865470a
Jake Moritz <jakemoritz1@gmail.com>
2016-10-16 09:12:11 -0400

    Implemented empty view for BacklogFragment

--- app/src/main/java/me/jakemoritz/animebuzz/fragments/BacklogFragment.java
+++ app/src/main/java/me/jakemoritz/animebuzz/fragments/BacklogFragment.java
@@ -10,16 +10,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.util.Collections;
-
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
 import me.jakemoritz.animebuzz.R;
 import me.jakemoritz.animebuzz.activities.MainActivity;
 import me.jakemoritz.animebuzz.adapters.BacklogRecyclerViewAdapter;
 import me.jakemoritz.animebuzz.helpers.App;
-import me.jakemoritz.animebuzz.helpers.comparators.BacklogItemComparator;
 import me.jakemoritz.animebuzz.interfaces.mal.IncrementEpisodeCountResponse;
+import me.jakemoritz.animebuzz.models.BacklogItem;
 
 public class BacklogFragment extends Fragment implements IncrementEpisodeCountResponse {
 
@@ -28,6 +29,8 @@ public class BacklogFragment extends Fragment implements IncrementEpisodeCountRe
     private BacklogRecyclerViewAdapter mAdapter;
     private View backlogLayout;
     private MainActivity mainActivity;
+    private RecyclerView recyclerView;
+    private RelativeLayout emptyView;
 
     public BacklogRecyclerViewAdapter getmAdapter() {
         return mAdapter;
@@ -57,28 +60,42 @@ public class BacklogFragment extends Fragment implements IncrementEpisodeCountRe
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
-        container.removeAllViews();
-        container.clearDisappearingChildren();
-
-//        FragmentBacklogBinding backlogBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_backlog, container, false);
+//        container.removeAllViews();
+//        container.clearDisappearingChildren();
 
         backlogLayout = inflater.inflate(R.layout.fragment_backlog, container, false);
+        recyclerView = (RecyclerView) backlogLayout.findViewById(R.id.list);
+        emptyView = (RelativeLayout) backlogLayout.findViewById(R.id.empty_view_included);
 
-        TextView emptyText = (TextView) backlogLayout.findViewById(R.id.empty_text);
+        TextView emptyText = (TextView) emptyView.findViewById(R.id.empty_text);
         emptyText.setText(getString(R.string.empty_text_backlog));
 
         Context context = backlogLayout.getContext();
-        RecyclerView recyclerView = (RecyclerView) backlogLayout.findViewById(R.id.backlog);
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
-        Collections.sort(App.getInstance().getBacklog(), new BacklogItemComparator());
-        mAdapter = new BacklogRecyclerViewAdapter(this, App.getInstance().getBacklog());
+        RealmResults<BacklogItem> backlogItems = App.getInstance().getBacklog().sort("alarmTime");
+        setVisibility(backlogItems);
+        backlogItems.addChangeListener(new RealmChangeListener<RealmResults<BacklogItem>>() {
+            @Override
+            public void onChange(RealmResults<BacklogItem> element) {
+                setVisibility(element);
+            }
+        });
+        mAdapter = new BacklogRecyclerViewAdapter(this, backlogItems);
         mAdapter.touchHelper.attachToRecyclerView(recyclerView);
 
         recyclerView.setAdapter(mAdapter);
 
-//        backlogBinding.setData(dataset);
-
         return backlogLayout;
+    }
+
+    private void setVisibility(RealmResults<BacklogItem> element) {
+        if (element.isEmpty() && recyclerView.getVisibility() == View.VISIBLE) {
+            recyclerView.setVisibility(View.GONE);
+            emptyView.setVisibility(View.VISIBLE);
+        } else if (!element.isEmpty() && emptyView.getVisibility() == View.VISIBLE) {
+            recyclerView.setVisibility(View.VISIBLE);
+            emptyView.setVisibility(View.GONE);
+        }
     }
 
     @Override
