87c92d75999c8b74d50c056c259e0ba382a3b884
Jake Moritz <jakemoritz1@gmail.com>
2017-01-08 19:40:27 -0500

    Fixed images not downloading

--- app/src/main/java/me/jakemoritz/animebuzz/services/PosterDownloader.java
+++ app/src/main/java/me/jakemoritz/animebuzz/services/PosterDownloader.java
@@ -3,13 +3,15 @@ package me.jakemoritz.animebuzz.services;
 import android.app.IntentService;
 import android.content.Intent;
 import android.graphics.Bitmap;
-
-import com.squareup.picasso.Picasso;
+import android.graphics.BitmapFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import me.jakemoritz.animebuzz.helpers.App;
 
@@ -26,17 +28,36 @@ public class PosterDownloader extends IntentService {
 
         Bitmap bitmap = null;
         try {
-            bitmap = Picasso.with(App.getInstance()).load(URL).get();
-            cachePoster(bitmap, MALID);
+            bitmap = getBitmapFromURL(URL);
+
+            if (bitmap != null){
+                cachePoster(bitmap, MALID);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public Bitmap getBitmapFromURL(String url) throws IOException {
+        Bitmap bitmap;
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestProperty("User-Agent",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
+        connection.connect();
+        InputStream input = connection.getInputStream();
+
+        bitmap = BitmapFactory.decodeStream(input);
+
+        input.close();
+        connection.disconnect();
+        return bitmap;
+    }
+
     private void cachePoster(Bitmap bitmap, String MALID) {
         FileOutputStream fos = null;
         try {
-            File file = new File(App.getInstance().getFilesDir(), MALID + ".jpg");
+            File file = new File(App.getInstance().getCacheDir(), MALID + ".jpg");
             fos = new FileOutputStream(file);
             bitmap.compress(Bitmap.CompressFormat.JPEG, 25, fos);
         } catch (FileNotFoundException e) {
