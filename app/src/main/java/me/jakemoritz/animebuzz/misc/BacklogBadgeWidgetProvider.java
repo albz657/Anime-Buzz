ff9abef8ae7373f47168f0cc00f0d5933ea3f68c
Jake Moritz <jakemoritz1@gmail.com>
2017-01-28 16:24:18 -0500

    Started implementing backlog badge widget

---
+++ app/src/main/java/me/jakemoritz/animebuzz/misc/BacklogBadgeWidgetProvider.java
@@ -0,0 +1,46 @@
+package me.jakemoritz.animebuzz.misc;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class BacklogBadgeWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        for (int i = 0; i < appWidgetIds.length; i++){
+            int appWidgetId = appWidgetIds[i];
+
+
+        }
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+}
