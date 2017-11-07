678d7868bf2fa8b8bd112ac871647f242e65ea09
Jake Moritz <jakemoritz1@gmail.com>
2017-11-07 16:36:38 -0500

    Check 'finished setup' state in HelperActivity

--- app/src/main/java/me/jakemoritz/animebuzz/activities/HelperActivity.java
+++ app/src/main/java/me/jakemoritz/animebuzz/activities/HelperActivity.java
@@ -1,8 +1,14 @@
 package me.jakemoritz.animebuzz.activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.f2prateek.rx.preferences2.RxSharedPreferences;
+
+import me.jakemoritz.animebuzz.utils.Constants;
 
 /**
  * This is a small Activity that will controls the app launch flow. It will either launch into the
@@ -14,9 +20,13 @@ public class HelperActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // TODO: Use SharedPrefs variable to decide if user finished setup
-        if (false) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        RxSharedPreferences rxPrefs = RxSharedPreferences.create(sharedPreferences);
+
+        if (rxPrefs.getBoolean(Constants.SHARED_PREF_KEY_FINISHED_SETUP, false).get()) {
             // User completed setup, proceed into app
+            finish();
+            startActivity(MainActivity.newIntent(this));
         } else {
             // User has not completed setup, proceed into setup flow
             finish();
